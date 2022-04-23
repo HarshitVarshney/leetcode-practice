eaf310de8c52803a0ace40fda69dcf9f77c5a833
HarshitVarshney <hv.harshit@gmail.com>
2022-04-24 02:07:40 +0530

    Time: 3 ms (96.17%), Space: 46.5 MB (52.18%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.java
@@ -14,50 +14,31 @@
  * }
  */
 class Solution {
-//     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-//         if (root == null) return false;
-//         if (checkIftreesAreSame(root, subRoot)) {
-//             return true;
-//         } else {
-//             return checkIftreesAreSame(root.left, subRoot) || checkIftreesAreSame(root.right, subRoot);
-//         }
-//     }
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null) return false;
+        if (checkIftreesAreSame(root, subRoot)) {
+            return true;
+        } 
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
     
-//     Boolean checkIftreesAreSame (TreeNode startroot, TreeNode subRoot) {
-        
-//         if (startroot == null && subRoot == null) {
-//             return true;
-//         }
-        
-//         if (startroot == null || subRoot == null) {
-//             return false;
-//         } 
-        
-//         if (startroot.val != subRoot.val) {
-//             return false;
-//         }
-        
-//         return checkIftreesAreSame(startroot.left, subRoot.left) && checkIftreesAreSame(startroot.right, subRoot.right);
-        
-        
-        
-//     }
-    
-    public boolean isSubtree(TreeNode s, TreeNode t) {
-        if (s == null) return false;
-        if (isSame(s, t)) return true;
-        return isSubtree(s.left, t) || isSubtree(s.right, t);
     }
     
-    private boolean isSame(TreeNode s, TreeNode t) {
-        if (s == null && t == null) return true;
-        if (s == null || t == null) return false;
+    boolean checkIftreesAreSame (TreeNode startroot, TreeNode subRoot) {
         
-        if (s.val != t.val) return false;
+        if (startroot == null && subRoot == null) {
+            return true;
+        }
         
-        return isSame(s.left, t.left) && isSame(s.right, t.right);
+        if (startroot == null || subRoot == null) {
+            return false;
+        } 
+        
+        if (startroot.val != subRoot.val) {
+            return false;
+        }
+        
+        return checkIftreesAreSame(startroot.left, subRoot.left) && checkIftreesAreSame(startroot.right, subRoot.right);
     }
-    
-    
+        
 }
 
