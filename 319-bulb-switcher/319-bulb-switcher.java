8ddca981440fdec7e72a2a457f6531d34cfa9a8a
HarshitVarshney <hv.harshit@gmail.com>
2022-05-02 16:49:53 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (50.26%) - LeetHub

---
+++ 319-bulb-switcher/319-bulb-switcher.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int bulbSwitch(int n) {
+        
+        return (int)Math.sqrt(n);
+    }
+}
+
+
+
+// 0 0 0 0 0 
+// 1 1 1 1 1
+// 1 0 1 0 1
+// 1 0 0 0 0
+// 1 0 0 1 0 
+// 1 0 0 1 1    
+    
+    
+    
+    
